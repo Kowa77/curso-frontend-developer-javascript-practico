98e1f742adfa1993bd84a462a8c3f9ca27a875b4
Esteban Garriga <estebandavidgarriga@hotmail.com>
2023-06-19 05:00:34 -0300

    Add interacciones DOM basicas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,92 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const inactive = document.querySelector('.inactive');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const mainContainer = document.querySelector('.main-container');
 
 
-navEmail.addEventListener('click', mostrar );
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamIcon = document.querySelector('.menu');
+const aside = document.querySelector('.product-detail');
 
-function mostrar(){
-    desktopMenu.classList.toggle('inactive');
+
+navEmail.addEventListener('click', toggleEmailMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
+
+function toggleEmailMenu(){
+    desktopMenu.classList.toggle('inactive'); 
+    aside.classList.add('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleCartAside(){
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
+const productList = [];
+productList.push({
+    nombre: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'Termo',
+    price: 70,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'Mate',
+    price: 40,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+const cardscontainer = document.querySelector('.cards-container');
+function renderProducts(arr) {
+    for (product of arr){
+        
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+      
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+      
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+      
+        const productInfoDiv = document.createElement('div');
+      
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+      
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+      
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+      
+        productInfoFigure.appendChild(productImgCart);
+      
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+      
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+      
+        cardscontainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
+
+
